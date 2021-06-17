269dbb31fd7f6665edabcbe36ec5f5af58562c7f
ahmaddroobi99 <ahmad.droobi1999@gmail.com>
2021-06-17 10:40:03 +0300

    hi

--- conditional_staments.py
+++ conditional_staments.py
@@ -8,3 +8,14 @@ def find_averages(xlist):
 
 zlist = [[20, 15, 40], [], [5, 8, 10, 15, 100], [3.14]]
 find_averages(zlist)
+
+creatures = [['dog', 'Rover', 7], ['Fred','human', 2],
+  ['dog', 'Fido', 1], ['humam', 'Sally', 32]]
+for species, name, age in creatures:
+   if species == 'dog':
+       if age <= 2:
+             print(name, "is just a pup.")
+       if age > 2:
+            print(name, "is all grown up.")
+   if species == 'humam':
+          print("Hi " + name + ".")
